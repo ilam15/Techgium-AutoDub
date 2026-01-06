b740914343e8d5a13bed3a96b2e7b435651cf162
ilam15 <ilamsaravan@gmail.com>
2026-01-06 11:51:57 +0530

    feat: Initialize React frontend with Vite, TailwindCSS, and core authentication components.

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,11 @@
+import Login from "./components/authentication/Login";
+import Register from "./components/authentication/Register";
 
 function App() {
- 
-
   return (
     <>
-      
+      <Login />
+      <Register />
     </>
   )
 }
